fb2dc970d5a67ab368e0d9ecafb145f07a61bc79
RIhab212 <80919317+RIhab212@users.noreply.github.com>
2023-03-17 09:11:39 +0100

    Add files via upload

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - prints exactly with proper grammar, but the outcome 
- * is a piece of art,, followed by a new line.
- * Return: 0 if exited properly, 
- */
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
